495aa1d2160ce436200258de327dfcbbcf580de0
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-09-23 21:42:07 -0700

    add more segments, still can't figure out why my startQuiz function won't work

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // variable list
 
 var highScoreElement = document.getElementById('#highScore');
-var startBtn = document.getElementById("btnStart");
-var start = document.getElementById("#startpage")
+var timerEl = document.getElementById("timer")
+
+
+var correctDisplay = document.getElementById("#correctBar");
+var incorrectDisplay = document.getElementById("#wrongBar");
+
+var startBtn = document.getElementById("startBtn");
+var start = document.getElementById("start");
+var recScore = document.getElementById("recordScore")
+var endScore = document.getElementById("endScore")
+var question = document.getElementById("question")
+
+var answerA = document.getElementById("#btnA");
+var answerB = document.getElementById("#btnB");
+var answerC = document.getElementById("#btnC");
+var answerD = document.getElementById("#btnD");
+
 var startTime = 90;
 var timerInterval = -1;
+var endingScore = document.getElementById("endingScore")
 
-var correctDisplay = document.getElementById("#correctBar")
-var incorrectDisplay = document.getElementById("#wrongBar")
 
-var answerA = document.getElementById("#btnA")
-var answerB = document.getElementById("#btnB")
-var answerC = document.getElementById("#btnC")
-var answerD = document.getElementById("#btnD")
-
-var currentQuestionIndex = 0;
-var lastQuestionIndex = questionList.length-1;
 
 //define quiz q/a
 var questionList = [
@@ -40,46 +47,34 @@ var questionList = [
 
 
 // quiz sequence
-function showCurrentQuestion() {
-    var question = currentQuestion[currentQuestionIndex]
+function currentQuestion() {
+    var q = questionList[currentQuestionIndex]
 
-    quizQuestion.textContext = question.quizQuestion
-    answerA.textContext = question.answerA
-    answerB.textContext = question.answerB
-    answerC.textContext = question.answerC
-    answerD.textContext = question.answerD
+    quizQuestion.textContext = q.quizQuestion
+    answerA.textContext = q.answerA
+    answerB.textContext = q.answerB
+    answerC.textContext = q.answerC
+    answerD.textContext = q.answerD
 
 }
 
+var currentQuestionIndex = 0;
+var lastQuestionIndex = questionList.length-1;
 
 
-startBtn.addEventListener("click", quizStart());
+start.addEventListener("click", startQuiz());
 
-function quizStart(){
-    startBtn.style.display="none";
+function startQuiz(){
+    start.style.display="none";
     quiz.style.display="block";
-    showCurrentQuestion();
+    currentQuestion();
     quizTimer();
-}
-
-function quizTimer(){;
-    var timerInterval = setInterval(function(){
-        for (i = 0; i < timeRemaining; i++) {
-            if(timeRemaining <= 0){
-                clearInterval(timerInterval);
-                timeElement.textContext = "Time: 0";
-            }
-            else{
-                timeElement.textContext = "Time: " + timeRemaining;
-            }
-            timeRemaining-=1;
-        }
-    }, 1000);
+    
 }
 
 
     function clickAnswer(answer) {
-        if (answer === questions[currentQuestionIndex].correct){
+        if (answer === questionList[currentQuestionIndex].correct){
           correctDisplay.style.display="block";
           setTimeout(function () {
             correctDisplay.style.display='none';
@@ -88,13 +83,13 @@ function quizTimer(){;
         else {
           incorrectDisplay.style.display="block";
           setTimeout(function () {
-            noBar.style.display='none';
+            incorrectDisplay.style.display='none';
           }, 1000);
           timeLeft -=10
         }
         if(currentQuestionIndex < lastQuestionIndex) {
           currentQuestionIndex++;
-          showCurrentQuestion();
+          currentQuestion();
         }
         else {
           yourScore();
@@ -103,78 +98,97 @@ function quizTimer(){;
       }
 
 
+      function quizTimer(){;
+        var timerInterval = setInterval(function(){
+            for (i = 0; i < timeRemaining; i++) {
+                if(timeRemaining <= 0){
+                    clearInterval(timerInterval);
+                    timeElement.textContext = "Time: 0";
+                }
+                else{
+                    timeElement.textContext = "Time: " + timeRemaining;
+                }
+                timeRemaining-=1;
+            }
+        }, 1000);
+    }
+
 function yourScore(){
     clearInterval(timerInterval);
     timerInterval = -1;
     var timeScore = timeRemaining;
-    document.finalScore.innerHTML= "Your final score is: " + endScore
+    document.endingScore.innerHTML= "Your score is: " + timeScore
     timerEl.innerHTML="Timer: 0"
     quiz.style.display="none";
     recScore.style.display="block";
 
 }
 
-// function storeScore(){
 
-// }
+function recordScore() {
+    recScore.style.display="none";
+    endScore.style.display="block";
+    start.style.display="none"
+    quiz.style.display="none"
+    var scoreText = scoreInput.value.trim();
+    if (scoreText === "") {
+      return;
+    }
+    scores.push(scoreText + ", " + endingScore.textContent);
+    scoreInput.value = "";
+    scoreStorage();
+    showScores();
+  }
 
-// var questionPromptContainer = $("#questionPrompt")
-// ​
-// var questionChoicesContainer = $("#questionChoices")
-// ​
-// var questions = [
-//     {
-//         question: "Is the sky blue?",
-//         choices: ["Yes", "No"],
-//         answerIndex: 0
-//     }, {}, {}
-// ]
-// ​
-// var currentQuestionIndex = 0
-// ​
-// function handleStartingGame() {
-//     // Start timer
-    
-//     // Hide start page
-    
-//     // Show main game page
-    
-//     // Show current question
-//     showCurrentQuestion()
-// }
-// ​
-// function handleEndingGame() {
-//     // Hide main game view
-    
-//     // Show high score input view
-// }
-// ​
-// function showCurrentQuestion() {
-//     var currentQ = questions[currentQuestionIndex]
-    
-//     if(currentQ == null) {
-//         handleEndingGame()
-//         return;
-//     }
-    
-//     questionPromptContainer.empty()
-    
-//     questionPromptContainer.text(currentQ.question)
-    
-//     questionChoicesContainer.empty()
-    
-//     currentQ.choices.forEach(function(choice) {
-//         questionChoicesContainer.append($("<button>" + choice + "</button>"))
-//     })
-// }
-// ​
-// $(".answer-button").on('click', function() {
-//     // Check if the answer is correct or incorrect
-    
-//     // Show the user somehow (CSS classes)
-    
-//     // Increment Question Index
-//     currentQuestionIndex++
- 
-//     // Show user the current question
-//     showCurrentQuestion()
+
+var scoreInput = document.querySelector("#scoretext");
+var listScore = document.querySelector("#score-list");
+var scoreForm = document.querySelector("#score-form");
+
+
+var scores = [];
+
+loadScoreString();
+
+function showScores() {
+  listScore.innerHTML = "";
+
+  for (var i = 0; i < scores.length; i++) {
+    var score = scores[i];
+    var li = document.createElement("li");
+    li.textContent = score;
+    li.setAttribute("data-index", i);
+    listScore.appendChild(li);
+  }
+}
+
+function loadScoreString() {
+  var scoreStorage = JSON.parse(localStorage.getItem("scores"));
+  if (scoreStorage !== null) {
+    scores = scoreStorage;
+  }
+  showScores();
+}
+
+function scoreStorage() {
+  localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+scoreForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  return
+});
+
+
+
+function clearScores() {
+  localStorage.clear();
+  scores = []
+  scoreStorage();
+  showScores();
+}
+
+
+function restartQuiz() {
+  location.reload();
+}
